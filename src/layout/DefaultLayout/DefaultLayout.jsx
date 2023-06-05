5b38ec924f6c53dbacb52ac137d2b126fc642ea7
quythanh <quythanh.dev@gmail.com>
2023-06-05 23:33:16 +0700

    init proj

---
+++ src/layout/DefaultLayout/DefaultLayout.jsx
@@ -0,0 +1,13 @@
+import Header from '@/Header';
+import Footer from '@/Footer';
+
+const DefaultLayout = () => {
+    return (
+        <>
+            <Header />
+            <Footer />
+        </>
+    );
+};
+
+export default DefaultLayout;
