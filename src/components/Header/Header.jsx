cca06fc8153389343b8a5e830deb7de7832c2962
quythanh <quythanh.dev@gmail.com>
2023-06-08 15:49:51 +0700

    done header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,63 @@ import Button from '@/Button';
 
 const cx = classNames.bind(styles);
 
+const MENU_ITEM = [
+    {
+        icon: 'apps',
+        title: 'Tất cả môn học',
+        subItem: [],
+    },
+    {
+        icon: 'business_center',
+        title: 'Việc kinh doanh',
+        subItem: [],
+    },
+    {
+        icon: 'public',
+        title: 'Nhân văn',
+        subItem: [],
+    },
+    {
+        icon: 'calculate',
+        title: 'Toán học',
+        subItem: [],
+    },
+    {
+        icon: 'terminal',
+        title: 'Lập trình',
+        subItem: ['Lập trình C/C++', 'Lập trình Python', 'Lập trình Java', 'Lập trình JavaScript'],
+    },
+    {
+        icon: 'science',
+        title: 'Khoa học',
+        subItem: [],
+    },
+    {
+        icon: 'create',
+        title: 'Viết',
+        subItem: [],
+    },
+];
+
+const SubMenu = ({ data }) => {
+    return (
+        <li className={cx('sub-menu')}>
+            <div className="d-flex align-items-center pb-2">
+                {data.icon ? <span className="material-icons">{data.icon}</span> : null}
+                <span className="ms-1">{data.title}</span>
+                {data.subItem ? <span class="material-icons">arrow_drop_down</span> : null}
+            </div>
+            <ul>
+                {data.subItem.map((title, index) => (
+                    <li key={index} className="p-1 ps-3">
+                        {title}
+                    </li>
+                ))}
+            </ul>
+        </li>
+    );
+};
+
 const Header = () => {
     const [user, setUser] = useState(false);
     return (
@@ -15,8 +72,15 @@ const Header = () => {
                     <img src="src/assets/logo.png" alt="Logo" className="w-100 h-100" />
                 </div>
                 <div className={cx('input', 'd-flex align-items-center')}>
-                    <input type="text" placeholder="Tim kiem tren tai lieu" />
-                    <span className="material-icons">search</span>
+                    <select className="form-select">
+                        <option value="1">Các tài liệu</option>
+                        <option value="2">Câu hỏi</option>
+                        <option value="3">Giáo sư</option>
+                    </select>
+                    <input type="text" placeholder="Tim kiem tren tai lieu" className="form-control" />
+                    <button className="btn">
+                        <span className="material-icons">search</span>
+                    </button>
                 </div>
                 <div className={cx('actions')}>
                     <Button className="me-5">Tải lên</Button>
@@ -43,56 +107,10 @@ const Header = () => {
                     )}
                 </div>
             </header>
-            <ul className={cx('menu')}>
-                <li>
-                    Tat ca mon hoc
-                    <ul>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                </li>
-                <li>
-                    Viec kinh doanh
-                    <ul>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                </li>
-                <li>
-                    Nhan van
-                    <ul>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                </li>
-                <li>
-                    Toan hoc
-                    <ul>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                </li>
-                <li>
-                    Lap trinh
-                    <ul>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                </li>
-                <li>
-                    Khoa hoc
-                    <ul>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                </li>
-                <li>
-                    Viet
-                    <ul>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                </li>
+            <ul className={cx('menu', 'd-flex justify-content-around mt-2')}>
+                {MENU_ITEM.map((item, index) => (
+                    <SubMenu key={index} data={item} />
+                ))}
             </ul>
         </>
     );
