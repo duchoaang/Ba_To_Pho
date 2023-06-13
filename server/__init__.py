5ea9c810b31c098eb18b31f2552ba18990f8e474
nganbao0206 <yuu.mmd02@gmail.com>
2023-06-13 22:53:37 +0700

    update

--- server/__init__.py
+++ server/__init__.py
@@ -32,7 +32,7 @@ app.config['MAIL_DEFAULT_SENDER'] = 'tailieubatopho@gmail.com'
 app.config['SESSION_COOKIE_SAMESITE'] = 'None'
 app.config['SESSION_COOKIE_SECURE'] = True
 
-app.config["SESSION_PERMANENT"] = True
+app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
